98fffc6eaf7d77d63ca73e1fad714bb130f6e631
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-09-27 16:26:30 -0300

    feat: add index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export * from './sourcery/sourcery.module';
-export * from './sourcery/sourcery.service';
+export * from './base/base.input';
+export * from './base/base.output';
+export * from './base/base.router';
+export * from './base/base.use-case';
+
+export * from './mock/mock-config.service';
+export * from './mock/mock.repository';
+export * from './mock/mock.use-case';
